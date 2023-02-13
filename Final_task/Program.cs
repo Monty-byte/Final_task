fbe2a66adfef34260bcba30c0bb03906f785ef0f
Anton <kondalov92@gmail.com>
2023-02-13 17:00:27 +0400

    Получение массива строк от пользователя

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите количество слов, которые Вы хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(size);
+
+string[] array = new string[size];
+
+Console.WriteLine("Введите любые слова или символы через Enter: ");
+for (int i = 0; i < size; i++)
+{
+    array[i] = Console.ReadLine() ?? "";
+}
